1f65780c44ebe302dcc6ac2c48e08d01e2984e89
moralesmicaella <morales.micaella@gmail.com>
2024-04-01 15:14:34 -0700

    Initial Commit

---
+++ QueenlyTryOn/QueenlyTryOn.h
@@ -0,0 +1,18 @@
+//
+//  QueenlyTryOn.h
+//  QueenlyTryOn
+//
+//  Created by Mica Morales on 4/1/24.
+//
+
+#import <Foundation/Foundation.h>
+
+//! Project version number for QueenlyTryOn.
+FOUNDATION_EXPORT double QueenlyTryOnVersionNumber;
+
+//! Project version string for QueenlyTryOn.
+FOUNDATION_EXPORT const unsigned char QueenlyTryOnVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <QueenlyTryOn/PublicHeader.h>
+
+
